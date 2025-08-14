c222e54a81a1808180e7492199cc08c4701c69ac
Ashutosh Sinha <Ashutosh-Sinha@outlook.com>
2025-08-15 00:53:06 +0530

    Completed Snake movement Logic

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ document.addEventListener('DOMContentLoaded', function () {
     let food = { x: 300, y: 200 }; // {x: 15*20, y: 10*20} // -> cell coordinate -> pixels// top left pixels for food
     let snake = [{x: 160, y: 200}, {x: 140, y: 200}, {x: 120, y: 200}]; // [head, body, body, tail]
 
-     let dx = cellSize; // +20
+    let dx = cellSize; // +20
     let dy = 0;
+    let intervalId;
+    let gameSpeed = 200;
+    
+    function moveFood() {
+        let newX, newY;
+
+        do {
+            newX = Math.floor(Math.random() * 30) * cellSize;
+            newY = Math.floor(Math.random() * 30) * cellSize;
+        } while(snake.some(snakeCell => snakeCell.x === newX && snakeCell.y === newY));
+
+        food = { x: newX, y: newY };
+    }
 
     function updateSnake() {
         const newHead = { x: snake[0].x + dx, y: snake[0].y + dy };
@@ -18,7 +31,13 @@ document.addEventListener('DOMContentLoaded', function () {
         // check collision with food
         if(newHead.x === food.x && newHead.y === food.y) {
             score += 10;
-            // TODO: move food
+            moveFood();
+
+            if(gameSpeed > 50) {
+                clearInterval(intervalId);
+                gameSpeed -= 10;
+                gameLoop();
+            }
 
         } else {
             snake.pop(); // Remove tail
@@ -66,14 +85,36 @@ document.addEventListener('DOMContentLoaded', function () {
         const foodElement = drawDiv(food.x, food.y, 'food');
         gameArena.appendChild(foodElement);
     }
+      function isGameOver() {
+        // snake collision checks
+        for(let i = 1; i < snake.length; i++) {
+            if(snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+                return true;
+            }
+        }
 
-    function gameLoop() {
-        setInterval(() => {
-            updateSnake();
-            drawFoodAndSnake();
-        }, 150);
+        // wall collision checks
+        const hitLeftWall = snake[0].x < 0; // snake[0] -> head
+        const hitRightWall = snake[0].x > arenaSize - cellSize;
+        const hitTopWall = snake[0].y < 0;
+        const hitBottomWall = snake[0].y > arenaSize - cellSize;
+        return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall;
     }
 
+    function gameLoop() {
+         intervalId = setInterval(() => {
+            if(isGameOver()) {
+                clearInterval(intervalId);
+                gameStarted = false;
+                alert('Game Over' + '\n' + 'Your Score: ' + score);
+                return;
+            }
+            updateSnake();
+            drawFoodAndSnake();
+            drawScoreBoard();
+        }, gameSpeed);
+    };
+    
         function runGame() {
             if(!gameStarted) {
                 gameStarted = true;
@@ -81,6 +122,12 @@ document.addEventListener('DOMContentLoaded', function () {
             
                 gameLoop(); // TODO: Implement game loop
             }
+    }
+
+    
+    function drawScoreBoard() {
+        const scoreBoard = document.getElementById('score-board');
+        scoreBoard.textContent = `Score: ${score}`;
     }
 
     function initiateGame() {
